14c81591ed7a339bb9ddcc2025d1f6fb66ba68d2
Jens Ulferts <j.ulferts@finn.de>
2018-03-05 08:01:05 +0100

    squash all migrations up to 7.1.0

--- db/migrate/20180305130811_remove_wiki_content_versions.rb
+++ db/migrate/20180305130811_remove_wiki_content_versions.rb
@@ -27,20 +27,24 @@
 # See docs/COPYRIGHT.rdoc for more details.
 #++
 
-class GeneralizeWikiMenuItems < ActiveRecord::Migration[5.1]
+class RemoveWikiContentVersions < ActiveRecord::Migration[5.1]
   def up
-    rename_table :wiki_menu_items, :menu_items
-    add_column :menu_items, :type, :string
-    rename_column :menu_items, :wiki_id, :navigatable_id
-
-    MenuItem.find_each do |menu_item|
-      menu_item.update_attribute :type, 'MenuItems::WikiMenuItem'
-    end
+    drop_table :wiki_content_versions
   end
 
   def down
-    rename_table :menu_items, :wiki_menu_items
-    remove_column :wiki_menu_items, :type
-    rename_column :wiki_menu_items, :navigatable_id, :wiki_id
+    create_table :wiki_content_versions, force: true do |t|
+      t.integer :wiki_content_id, null: false
+      t.integer :page_id, null: false
+      t.integer :author_id
+      t.binary :data, limit: 16.megabytes
+      t.string :compression, limit: 6, default: ''
+      t.string :comments, default: ''
+      t.datetime :updated_on, null: false
+      t.integer :version, null: false
+    end
+
+    add_index :wiki_content_versions, [:updated_on], name: 'index_wiki_content_versions_on_updated_on'
+    add_index :wiki_content_versions, [:wiki_content_id], name: 'wiki_content_versions_wcid'
   end
 end
